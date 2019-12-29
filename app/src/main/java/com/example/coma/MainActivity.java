1fb91f2a0896079bc9cd0160915be5a145ceecc3
truongduckhai <kd.kidr@gmail.com>
2019-12-29 20:41:53 +0700

    add some java code

--- app/src/main/java/com/example/coma/MainActivity.java
+++ app/src/main/java/com/example/coma/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.coma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String name = "Coma";
+        int year = 2019;
+        Log.d("COMA", name + year);
+        ArrayList<String> arrayName = new ArrayList<>();
+        arrayName.add("Name1");
+        arrayName.add("Name2");
+        arrayName.add("Name3");
+        for (int i = 0; i < arrayName.size(); i++) {
+            Log.d("COMA", arrayName.get(i));
+        }
+
     }
 }
