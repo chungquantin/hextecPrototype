8e2bac967246ba2e6ff1b741019ade3fca5463aa
truongduckhai <kd.kidr@gmail.com>
2020-01-02 17:40:52 +0700

    adding 2nd activity

--- app/src/main/java/com/example/coma/MainActivity.java
+++ app/src/main/java/com/example/coma/MainActivity.java
@@ -2,27 +2,33 @@ package com.example.coma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
-
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 5000;
+    TextView welcomeText;
+    Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name = "Coma";
-        int year = 2019;
-        Log.d("COMA", name + year);
-        ArrayList<String> arrayName = new ArrayList<>();
-        arrayName.add("Name1");
-        arrayName.add("Name2");
-        arrayName.add("Name3");
-        for (int i = 0; i < arrayName.size(); i++) {
-            Log.d("COMA", arrayName.get(i));
-        }
-
+        welcomeText = (TextView) findViewById(R.id.welcomeText);
+        configureStartButton();
+    }
+    private void configureStartButton(){
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this,InputActivity.class));
+            }
+        });
     }
 }
